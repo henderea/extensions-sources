6c82bacc5ef694b1a97a9020f3a22e9ca56a8bbb
Eric Henderson <henderea@gmail.com>
2022-04-23 13:33:07 -0400

    add my deploy script

---
+++ my-deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+name="${1:-$(git rev-parse --abbrev-ref HEAD)}"
+echo "sudo rm -rf '/home/henderea/domains/paperback/${name}'"
+sudo rm -rf "/home/henderea/domains/paperback/${name}"
+echo "sudo cp -r '/home/coder/projects/extensions-sources/bundles' '/home/henderea/domains/paperback/${name}'"
+sudo cp -r "/home/coder/projects/extensions-sources/bundles" "/home/henderea/domains/paperback/${name}"
+echo "sudo chown -R henderea:henderea /home/henderea/domains"
+sudo chown -R henderea:henderea "/home/henderea/domains"
